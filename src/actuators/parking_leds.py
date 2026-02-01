7fa4a2b255f06b0d2953b96fce350a629575fe23
AndreaBotta333 <anbotta2005@gmail.com>
2026-02-01 15:55:12 +0100

    Add ParkingLeds comments

--- src/actuators/parking_leds.py
+++ src/actuators/parking_leds.py
@@ -1,16 +1,43 @@
-# actuators/parking_leds.py
 import machine
 
 class ParkingLeds:
     def __init__(self, red_pin, green_pin):
+        # Inizializza due LED (rosso e verde) come uscite digitali.
+        #
+        # machine.Pin(..., machine.Pin.OUT) configura il pin come uscita digitale:
+        # - .on()  porta il pin a livello logico alto 
+        # - .off() porta il pin a livello logico basso
         self.red = machine.Pin(red_pin, machine.Pin.OUT)
         self.green = machine.Pin(green_pin, machine.Pin.OUT)
+
+        # Imposta lo stato iniziale del parcheggio come "libero":
+        # LED verde acceso, LED rosso spento.
         self.set_free()
         
     def set_occupied(self):
+        """
+        Imposta lo stato del parcheggio come "occupato".
+
+        A cosa serve:
+        - Segnalazione visiva locale della disponibilità (posto non disponibile).
+
+        Come funziona:
+        - Accende il LED rosso.
+        - Spegne il LED verde.
+        """
         self.red.on()
         self.green.off()
         
     def set_free(self):
+        """
+        Imposta lo stato del parcheggio come "libero".
+
+        A cosa serve:
+        - Segnalazione visiva locale della disponibilità (posto disponibile).
+
+        Come funziona:
+        - Spegne il LED rosso.
+        - Accende il LED verde.
+        """
         self.red.off()
         self.green.on()
